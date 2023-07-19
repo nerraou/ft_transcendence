2fa63391f91fed7784e98427bc04f7433e8063f9
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-19 10:43:40 +0100

    fix: use clsx package and reuse /outline/email component

--- frontend/src/components/atoms/icons/filled/Email.tsx
+++ frontend/src/components/atoms/icons/filled/Email.tsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import EmailOutline from "@icons/outline/Email";
+
 interface EmailProps {
   color: string;
   backgroundColor: string;
@@ -7,27 +10,13 @@ interface EmailProps {
 function Email(props: EmailProps) {
   return (
     <div
-      className={
-        props.backgroundColor +
-        " " +
-        "w-8 h-8 rounded-lg inline-flex justify-center items-center"
-      }
+      className={clsx(
+        props.backgroundColor,
+        "w-8 h-8 rounded-lg inline-flex justify-center items-center",
+      )}
       onClick={props.onClick}
     >
-      <svg
-        className={props.color}
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        strokeWidth="1"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-        <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
-        <path d="M3 7l9 6l9 -6"></path>
-      </svg>
+      <EmailOutline color={props.color} />
     </div>
   );
 }
