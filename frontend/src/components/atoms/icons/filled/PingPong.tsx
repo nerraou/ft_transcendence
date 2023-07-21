d0050c810b2e6924354cdf36cb14f8f78bebfe5d
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-21 12:16:16 +0100

    feat(icons): create filled and outline ping pong icons

---
+++ frontend/src/components/atoms/icons/filled/PingPong.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import PingPongOutline from "../outline/PingPong";
+import { MouseEvent } from "react";
+
+interface PingPongProps {
+  color: string;
+  backgroundColor: string;
+  onClick?: (event: MouseEvent<HTMLElement>) => void;
+}
+
+function PingPong(props: PingPongProps) {
+  return (
+    <div
+      className={clsx(
+        props.backgroundColor,
+        "w-8 h-8 rounded-lg inline-flex justify-center items-center",
+      )}
+      onClick={props.onClick}
+    >
+      <PingPongOutline color={props.color} />
+    </div>
+  );
+}
+
+export default PingPong;
