bbcf2d83e94c1d9421148ad1262355b5cee2c4e2
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-30 10:48:07 +0100

    feat: create match result card

---
+++ frontend/src/components/atoms/match-card/Player.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import clsx from "clsx";
+
+interface PlayerProps {
+  name: string;
+  state: "win" | "lose";
+  image: string;
+}
+
+interface PlayerNameProps {
+  name: string;
+}
+
+interface PlayerImageProps {
+  state: "win" | "lose";
+  image: string;
+}
+
+function PlayerImage(props: PlayerImageProps) {
+  return (
+    <Image
+      src={props.image}
+      alt="user image"
+      width="112"
+      height="112"
+      className={clsx(
+        "border-8 object-none rounded-full w-28 h-28 custom-position ",
+        {
+          "border-light-fg-tertiary": props.state == "win",
+          "border-light-fg-secondary": props.state == "lose",
+        },
+      )}
+    />
+  );
+}
+
+function PlayerName(props: PlayerNameProps) {
+  return (
+    <div className="flex justify-center items-center flex-col self-auto">
+      <div className="h-6 w-20 flex justify-center items-center bg-light-fg-link m-1 rounded-xl border-light-fg-primary border-2">
+        <label className="text-light-fg-tertiary text-base">{props.name}</label>
+      </div>
+      <div className="bg-light-fg-primary h-1.5 w-8"></div>
+    </div>
+  );
+}
+
+function Player(props: PlayerProps) {
+  return (
+    <div className="inline-flex justify-around items-center  box-border rounded-full border-light-fg-primary  bg-light-bg-secondary border-8 h-44 w-96">
+      <div>
+        <PlayerImage image={props.image} state={props.state} />
+        <PlayerName name={props.name} />
+      </div>
+      <div>
+        <PlayerImage image={props.image} state={props.state} />
+        <PlayerName name={props.name} />
+      </div>
+    </div>
+  );
+}
+
+export default Player;
