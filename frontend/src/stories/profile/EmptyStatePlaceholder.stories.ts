ae6ee6082e39e4913836e88d81bd8bec59046866
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-05 14:31:38 +0100

    feat: create empty state placeholder

---
+++ frontend/src/stories/profile/EmptyStatePlaceholder.stories.ts
@@ -0,0 +1,18 @@
+import EmptyStatePlaceholder from "@atoms/EmptyStatePlaceholder";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof EmptyStatePlaceholder> = {
+  title: "profile/EmptyStatePlaceholder",
+  component: EmptyStatePlaceholder,
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof EmptyStatePlaceholder>;
+
+export const Default: Story = {
+  args: {
+	color: "fill-light-useless"
+  },
+};
