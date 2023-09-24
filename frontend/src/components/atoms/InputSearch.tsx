11bc74c7d52353a8eeec2c913f5dee4076e449ca
Zridi Mahmoud <mahmoud.zridi@ocpsolutions.ma>
2023-09-24 11:56:20 +0100

    feat: create search input

---
+++ frontend/src/components/atoms/InputSearch.tsx
@@ -0,0 +1,44 @@
+import clsx from "clsx";
+import React, { ChangeEvent } from "react";
+import Search from "@icons/outline/Search";
+import X from "@icons/outline/X";
+
+interface InputSearchProps {
+  value: string;
+  placeholder: string;
+  textColor: string;
+  width?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputSearch = (props: InputSearchProps) => {
+  const defaultWidth = "w-64";
+  const width = props.width || defaultWidth;
+
+  return (
+    <div className={clsx("relative", width)}>
+      <div className="absolute top-1/2 left-3 transform -translate-y-1/2">
+        <Search className="text-light-fg-primary dark:text-dark-fg-primary" />
+      </div>
+      <input
+        type="text"
+        placeholder={props.placeholder}
+        value={props.value}
+        className={clsx(
+          props.textColor,
+          width,
+          "h-10 border-2 rounded-full bg-light-bg-tertiary outline-none focus:border-dark-useless px-5 pl-12 pr-10 border-light-fg-primary dark:border-dark-bg-primary",
+        )}
+        onChange={props.onChange}
+      />
+      <button
+        className="absolute top-1/2 right-3 transform -translate-y-1/2"
+        onClick={() => props.onChange({ target: { value: "" } } as any)}
+      >
+        <X className="text-light-fg-primary dark:text-dark-fg-primary" />
+      </button>
+    </div>
+  );
+};
+
+export default InputSearch;
---
+++ frontend/src/components/atoms/icons/outline/Search.tsx
@@ -0,0 +1,23 @@
+function Search({ className }: { className?: string }) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={className}
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+        <path d="M21 21l-6 -6"></path>
+      </svg>
+    );
+  }
+  
+  export default Search;
+  
