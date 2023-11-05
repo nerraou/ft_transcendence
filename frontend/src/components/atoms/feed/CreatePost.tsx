9d70b3b74d45428a095be777eb87a2d61baf2c5b
zridi-mahmoud <mahmoud.zridi0919@gmail.com>
2023-11-05 16:38:10 +0100

    feat(CreatePost): - complete the design - add file handling for image upload

--- frontend/src/components/atoms/feed/CreatePost.tsx
+++ frontend/src/components/atoms/feed/CreatePost.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import Send from "../icons/outline/Send";
+import Photo from "../icons/outline/Photo";
 
 interface CreatePostProps {
   avatar?: string;
@@ -10,28 +12,63 @@ const CreatePost = ({ avatar, onPost }: CreatePostProps) => {
   const [content, setContent] = useState("");
   const [image, setImage] = useState("");
 
+  const hiddenFileInput = useRef<HTMLInputElement>(null);
+
+  const handleFile = (file: File | undefined) => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileUploaded: File | undefined = event?.target?.files?.[0];
+    handleFile(fileUploaded);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center gap-8 rounded-lg px-0 py-[2px] border-2 shadow-light-lg border-light-fg-link dark:border-dark-bg-primary4 bg-light-bg-tertiary dark:bg-dark-fg-primary">
-      <div className="flex flex-row items-center justify-center">
+    <div className="flex flex-col items-start justify-center gap-8 rounded-lg px-8 py-4 border-2 shadow-light-lg border-light-fg-link dark:border-dark-bg-primary4 bg-light-bg-tertiary dark:bg-dark-fg-primary">
+      <div className="flex flex-row items-start justify-center gap-4 w-full">
         <Image
           src={avatar || "/default/user-circle.png"}
           alt="user avatar"
-          width={50}
-          height={50}
-          className="rounded-full"
+          width={0}
+          height={0}
+          sizes="50px"
+          className="rounded-full object-cover h-[50px] w-[50px]"
         />
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-row items-center justify-center">
-          <input
-            type="text"
+        <div className="flex flex-col items-start justify-center gap-8 w-full">
+          <textarea
+            cols={10}
+            className="w-full rounded-[10px] bg-light-fg-tertiary py-2 px-4 min-h-[50px] text-light-fg-link text-[18px] placeholder-light-fg-link"
             placeholder="What are you thinking?"
             onChange={(e) => setContent(e.target.value)}
           />
-        </div>
-        <div className="flex flex-row items-center justify-center">
-          <input type="file" onChange={(e) => setImage(e.target.value)} />
-          <button onClick={() => onPost(content, image)}>Post</button>
+          <div className="flex flex-row items-center justify-between w-full">
+            <button
+              onClick={() => hiddenFileInput.current?.click()}
+              className="flex flex-row items-center justify-center gap-2 rounded-2xl bg-light-fg-secondary text-light-fg-tertiary h-10 w-24 border border-light-fg-link dark:border-dark-bg-primary4 dark:bg-dark-fg-primary"
+            >
+              <Photo className="w-4 h-4 text-light-bg-tertiary" />
+              Photo
+              <input
+                type="file"
+                onChange={handleChange}
+                ref={hiddenFileInput}
+                className="hidden"
+              />
+            </button>
+            <button
+              onClick={() => onPost(content, image)}
+              className="flex flex-row items-center justify-center gap-2 rounded-2xl bg-light-bg-secondary text-light-fg-tertiary h-10 w-24 border border-light-fg-link "
+            >
+              <Send className="w-4 h-4" />
+              Post
+            </button>
+          </div>
         </div>
       </div>
     </div>
