4a537cde6a8887e4bf32037dc5a7e04ace1837aa
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2023-11-06 13:42:13 +0100

    chore: fix update profile dto file name, transform email to lowercase

--- backend/src/modules/auth/dto/sign-up.dto.ts
+++ backend/src/modules/auth/dto/sign-up.dto.ts
@@ -1,7 +1,9 @@
+import { Transform } from "class-transformer";
 import { IsEmail, IsStrongPassword } from "class-validator";
 
 export class SignUpDto {
   @IsEmail()
+  @Transform(({ value }) => value?.toLowerCase())
   email: string;
 
   @IsStrongPassword()
