4ac9e7467bf1e9cb0c3061a9f9ebcbcf7463b744
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-12-04 16:42:36 +0100

    fix: fix modal warning

--- frontend/src/components/atoms/Modal.tsx
+++ frontend/src/components/atoms/Modal.tsx
@@ -14,55 +14,53 @@ function Modal(props: ModalProps) {
   const completeDevRef = useRef(null);
 
   return (
-    <>
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          initialFocus={completeDevRef}
-          className="relative z-10"
-          onClose={() => setIsOpen(false)}
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        initialFocus={completeDevRef}
+        className="relative z-10"
+        onClose={() => setIsOpen(false)}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-dark-fg-primary/60" />
-          </Transition.Child>
+          <div className="fixed inset-0 bg-dark-fg-primary/60" />
+        </Transition.Child>
 
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="flex flex-col items-center justify-center w-full max-w-max p-10 rounded-md bg-light-bg-tertiary border-solid border-2 border-light-fg-primary">
-                  <Dialog.Title className="text-xl text-light-fg-primary leading-6">
-                    {props.title}
-                  </Dialog.Title>
-                  <Dialog.Description className="text-lg text-light-fg-link mt-4">
-                    <p>{props.description}</p>
-                  </Dialog.Description>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="flex flex-col items-center justify-center w-full max-w-max p-10 rounded-md bg-light-bg-tertiary border-solid border-2 border-light-fg-primary">
+                <Dialog.Title className="text-xl text-light-fg-primary leading-6">
+                  {props.title}
+                </Dialog.Title>
+                <Dialog.Description className="text-lg text-light-fg-link mt-4">
+                  {props.description}
+                </Dialog.Description>
 
-                  <div className="mt-5" ref={completeDevRef}>
-                    {props.action}
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+                <div className="mt-5" ref={completeDevRef}>
+                  {props.action}
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
-        </Dialog>
-      </Transition>
-    </>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
 
