a3ad74d23f652bad960b3e49835f08002293e43f
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2023-12-21 20:23:56 +0100

    chore(image-cropper): fix types and make it controlled

--- frontend/src/components/molecules/Settings/ImageCroper.tsx
+++ frontend/src/components/molecules/Settings/ImageCroper.tsx
@@ -1,111 +1,102 @@
-import { ChangeEvent, useState } from "react";
-import { getCroppedImg, getRotatedImage } from "./ImageCroperCanvasUtils";
-import ImageDailog from "./ImageDailog";
+import { useEffect, useState } from "react";
+import { getCroppedImg } from "./ImageCroperCanvasUtils";
 import Cropper, { Area } from "react-easy-crop";
 import Button from "@components/atoms/Button";
-import { Orientation, getOrientation } from "get-orientation/browser";
 
-const ORIENTATION_TO_ANGLE: keyof Orientation = {
-  3: 180,
-  6: 90,
-  8: -90,
-};
+interface ImageCroperProps {
+  file: File | undefined;
+  onCropComplete: (image: Blob) => void;
+}
 
-export default function ImageCroper() {
+function readFile(file: File): Promise<string> {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      () => resolve(reader.result as string),
+      false,
+    );
+    reader.readAsDataURL(file);
+  });
+}
+
+export default function ImageCroper(props: ImageCroperProps) {
   const [imageSrc, setImageSrc] = useState<string | null>(null);
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [rotation, setRotation] = useState(0);
   const [zoom, setZoom] = useState(1);
   const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area | null>(null);
-  const [croppedImage, setCroppedImage] = useState<Blob | null>(null);
 
-  const onCropComplete = (area: Area, areaPixels: Area) => {
+  useEffect(() => {
+    if (props.file) {
+      readFile(props.file)
+        .then(setImageSrc)
+        .catch(() => {
+          // error
+        });
+    }
+  }, [props.file]);
+
+  function onCropComplete(area: Area, areaPixels: Area) {
     setCroppedAreaPixels(areaPixels);
-  };
+  }
 
-  const showCroppedImage = async () => {
+  async function showCroppedImage() {
+    if (!imageSrc || !croppedAreaPixels) {
+      return;
+    }
+
     try {
-      if (!imageSrc || !croppedAreaPixels) {
-        return;
-      } else {
-        const croppedImg = await getCroppedImg(
-          imageSrc,
-          croppedAreaPixels,
-          rotation,
-        );
-        setCroppedImage(croppedImg);
-      }
+      const croppendImage = await getCroppedImg(
+        imageSrc,
+        croppedAreaPixels,
+        rotation,
+      );
+      props.onCropComplete(croppendImage);
     } catch (e) {
       //   console.error(e);
     }
-  };
+  }
 
-  const onFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const file = e.target.files[0];
-      let imageDataUrl = await readFile(file);
-
-      try {
-        // apply rotation if needed
-        const orientation = await getOrientation(file);
-        const rotationAngle = ORIENTATION_TO_ANGLE[orientation];
-        if (rotation) {
-          imageDataUrl = await getRotatedImage(imageDataUrl, rotationAngle);
-          setImageSrc(imageDataUrl);
-        }
-      } catch {
-        console.warn("failed to detect the orientation");
-      }
-    }
-  };
+  if (!imageSrc) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
-      {imageSrc ? (
-        <>
-          <div>
-            <div className="relative w-96 h-96">
-              <Cropper
-                image={imageSrc}
-                crop={crop}
-                rotation={rotation}
-                zoom={zoom}
-                aspect={4 / 3}
-                onCropChange={setCrop}
-                onRotationChange={setRotation}
-                onCropComplete={onCropComplete}
-                onZoomChange={setZoom}
-              />
-            </div>
-            <input
-              type="range"
-              min={1}
-              max={3}
-              onChange={(e) => setZoom(e.target.value)}
-            />
+      <div className="relative w-96 h-96">
+        <Cropper
+          image={imageSrc}
+          crop={crop}
+          rotation={rotation}
+          zoom={zoom}
+          aspect={1}
+          onCropChange={setCrop}
+          onRotationChange={setRotation}
+          onCropComplete={onCropComplete}
+          onZoomChange={setZoom}
+        />
+      </div>
+      <input
+        type="range"
+        step={0.01}
+        min={1}
+        max={5}
+        value={zoom}
+        onChange={(e) => {
+          setZoom(parseFloat(e.currentTarget.value));
+        }}
+      />
 
-            <input
-              type="range"
-              min={0}
-              max={300}
-              onChange={(e) => setRotation(e.target.value)}
-            />
+      <input
+        type="range"
+        min={0}
+        max={180}
+        value={rotation}
+        onChange={(e) => setRotation(parseFloat(e.currentTarget.value))}
+      />
 
-            <Button text="Show" onClick={showCroppedImage} />
-          </div>
-          {croppedImage && <ImageDailog image={croppedImage} />}
-        </>
-      ) : (
-        <input type="file" onChange={onFileChange} accept="image/*" />
-      )}
+      <Button text="Show" onClick={showCroppedImage} />
     </div>
   );
-}
-
-function readFile(file: File): Promise<string | ArrayBuffer | null> {
-  return new Promise((resolve) => {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => resolve(reader.result), false);
-    reader.readAsDataURL(file);
-  });
 }
