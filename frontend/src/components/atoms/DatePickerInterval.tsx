ef024aee5c92ca49c3fae06849b2380c38e59d5a
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2023-12-25 00:28:42 +0100

    feat: create DatePicker and DatePickerInterval components

---
+++ frontend/src/components/atoms/DatePickerInterval.tsx
@@ -0,0 +1,63 @@
+import DatePicker, { DateValue } from "@atoms/DatePicker";
+import Calendar from "@icons/outline/Calendar";
+
+export type DateInterval = [DateValue, DateValue];
+
+interface DatePickerIntervalProps {
+  value: DateInterval | null | undefined;
+
+  onChange: (value: DateInterval) => void;
+}
+
+function getStartValue(value: DateInterval | null | undefined) {
+  if (value && value.length >= 1) {
+    return value[0];
+  }
+}
+
+function getEndValue(value: DateInterval | null | undefined) {
+  if (value && value.length == 2) {
+    return value[1];
+  }
+}
+
+export default function DatePickerInterval(props: DatePickerIntervalProps) {
+  function startDateChangeHandler(value: DateValue) {
+    let endDate: DateValue;
+
+    if (props.value && props.value.length == 2) {
+      endDate = props.value[1];
+    }
+
+    props.onChange([value, endDate]);
+  }
+
+  function endDateChangeHandler(value: DateValue) {
+    let startDate: DateValue;
+
+    if (props.value && props.value.length >= 1) {
+      startDate = props.value[0];
+    }
+
+    props.onChange([startDate, value]);
+  }
+
+  return (
+    <div className="inline-flex w-80 items-center border-2 border-light-fg-primary bg-light-fg-tertiary rounded-full px-4 h-10">
+      <Calendar margin="mr-2" />
+      <div className="flex h-full items-center flex-grow justify-evenly">
+        <DatePicker
+          buttonHeight="flex items-center min-h-full"
+          value={getStartValue(props.value)}
+          onChange={startDateChangeHandler}
+        />
+        <span className="inline-block w-1 mx-2 h-px bg-light-fg-primary" />
+        <DatePicker
+          buttonHeight="flex items-center min-h-full"
+          value={getEndValue(props.value)}
+          onChange={endDateChangeHandler}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/atoms/icons/outline/Calendar.tsx
@@ -0,0 +1,26 @@
+interface CalendarProps {
+  margin?: string;
+}
+
+export default function Calendar(props: CalendarProps) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={props.margin}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+      <path d="M16 3l0 4" />
+      <path d="M8 3l0 4" />
+      <path d="M4 11l16 0" />
+      <path d="M8 15h2v2h-2z" />
+    </svg>
+  );
+}
