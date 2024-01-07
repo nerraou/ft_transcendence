c38267f5d0e869110f8bcd088778312ff826afc5
zridi-mahmoud <mahmoud.zridi0919@gmail.com>
2024-01-07 16:45:06 +0100

    fix(landing): show feed page if user is logged

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,30 +8,58 @@ const GameBoard = dynamic(() => import("./game/make/components/GameBoard"), {
 import dynamic from "next/dynamic";
 import SittingLogo from "@components/atoms/icons/SittingLogo";
 import Logo from "@components/atoms/icons/Logo";
+import { useSession } from "next-auth/react";
+import LoadingPage from "./loading";
 
 export default function Home() {
+  const { status: sessionStatus } = useSession();
+
+  if (sessionStatus === "loading") {
+    return (
+      <LoadingPage
+        bgColor=" bg-light-bg-tertiary dark:bg-dark-bg-primary"
+        width="w-screen"
+        height="h-screen"
+      />
+    );
+  }
   return (
     <main className="w-max-w space-y-4 flex flex-col justify-center items-center p-8 text-xxl text-light-fg-primary dark:text-light-bg-tertiary bg-light-fg-tertiary dark:bg-dark-bg-primary">
       <h1 className="text-center dark:text-light-bg-tertiary text-light-fg-primarytext-xxl">
         Welcome To PongBoy
       </h1>
       <div className="flex space-x-96 md:space-x-40 sm:space-x-16">
-        <div className="flex flex-col">
-          <SittingLogo />
-          <Link href="/sign-up">
-            <button className="w-56 sm:w-full p-4 text-base text-center text-light-bg-tertiary bg-light-fg-link border rounded-lg border-light-fg-primary">
-              Create acount
-            </button>
-          </Link>
-        </div>
-        <div className="flex flex-col">
-          <Logo />
-          <Link href="/sign-in">
-            <button className="w-56 sm:w-full p-4 text-center text-base text-light-fg-link rounded-lg border border-light-fg-primary bg-light-bg-secondary">
-              Sign in
-            </button>
-          </Link>
-        </div>
+        {sessionStatus === "unauthenticated" ? (
+          <>
+            <div className="flex flex-col">
+              <SittingLogo />
+              <Link href="/sign-up">
+                <button className="w-56 sm:w-full p-4 text-base text-center text-light-bg-tertiary bg-light-fg-link border rounded-lg border-light-fg-primary">
+                  Create acount
+                </button>
+              </Link>
+            </div>
+            <div className="flex flex-col">
+              <Logo />
+              <Link href="/sign-in">
+                <button className="w-56 sm:w-full p-4 text-center text-base text-light-fg-link rounded-lg border border-light-fg-primary bg-light-bg-secondary">
+                  Sign in
+                </button>
+              </Link>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col">
+              <Logo />
+              <Link href="/feed">
+                <button className="w-56 sm:w-full p-4 text-center text-base text-light-fg-link rounded-lg border border-light-fg-primary bg-light-bg-secondary">
+                  Feed
+                </button>
+              </Link>
+            </div>
+          </>
+        )}
       </div>
       <GameBoard
         margin="mt-16 mb-16"
